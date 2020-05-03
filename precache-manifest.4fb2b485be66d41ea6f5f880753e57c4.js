1a859f30ca298ecb8974db6db7adc284b325aafb
Mark Graves <mark@myire.com>
2020-05-02 18:46:09 -0700

    jsonresume-typescript-react-serializer output

---
+++ precache-manifest.4fb2b485be66d41ea6f5f880753e57c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f492f07967bde50a0d8bd2443e16d59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cda1871ff08bc19f15e",
+    "url": "/static/js/2.e16b165c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e16b165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b57878a93490f677bc",
+    "url": "/static/js/main.fcca0fc5.chunk.js"
+  },
+  {
+    "revision": "e3b4ad671e81b2c7c706",
+    "url": "/static/js/runtime-main.9f4cb156.js"
+  }
+]);
